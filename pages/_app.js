27f4f2c4cc9a9c77e5fa1e18c4a2d24fcd8b9c06
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2022-12-27 20:11:51 +0000

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/globals.css'
+import { Hydrate, QueryClientProvider } from "react-query";
+
+import { queryClient } from "../src/api";
+import Layout from "../components/Layout";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
